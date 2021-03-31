85448623a49405b3ed913211726841786cb69134
wangzp <6010625@qq.com>
2021-03-31 21:06:45 +0800

    '引入computed'

--- miniprogram/pages/house-price/index.js
+++ miniprogram/pages/house-price/index.js
@@ -1,11 +1,32 @@
-// pages/house-price/index.js
+const computedBehavior = require('miniprogram-computed')
+
 Page({
+
+    behaviors: [computedBehavior.behavior],
 
     /**
      * 页面的初始数据
      */
     data: {
-        dealPrice: '',   // 成交价
+        dealPrice: 1,   // 成交价
+        area: 1,       // 面积
+        deposit: 20000,       // 定金
+        balance: 10000,       // 尾款
+        declarePrice: '',       // 申报价
+        loan: '',       // 贷款额
+        firstRate: 0.3,  // 首付比例
+        mediation: '',       // 中介费
+        loanService: '',       // 贷款服务费
+        deedTax: '',       // 契税
+        incomeTax: '',       // 个税
+        pureFirstPay: '',       // 净首付
+        totalFirstPay: '',       // 总首付
+    },
+
+    computed: {
+        unitPrice: function(data) {
+            return data.dealPrice / data.area
+        }
     },
 
     /**
@@ -58,9 +79,47 @@ Page({
     },
 
     /**
+     * 输入框失焦
+     */
+    onBlur(event) {
+        this.setData({
+            dealPrice: event.detail.value
+        })
+
+    },
+
+    /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
 
+    },
+
+    toTeacher() {
+        wx.navigateToMiniProgram({
+            appId: 'wxb76abad73a6c1d5c',
+            path: 'packageA/pages/teacher-center/teacher-center?lecCode=lec2702',
+            envVersion: 'develop',       // develop | trial | release
+            success() {
+                resolve()
+            },
+            fail() {
+                reject()
+            }
+        })
+    },
+
+    toLive() {
+        wx.navigateToMiniProgram({
+            appId: 'wxb76abad73a6c1d5c',
+            path: 'pages/swiper-live/swiper-live?roomID=room_lec2702_c40688',
+            envVersion: 'develop',       // develop | trial | release
+            success() {
+                resolve()
+            },
+            fail() {
+                reject()
+            }
+        })
     }
 })
